88ed0ce014c82f9f6a7889c6f21d2ca6916fc2b6
nquinbla <nquinbla@myuax.com>
2023-11-07 11:30:42 +0100

    condiciones
    cumplimiento de las condiciones del triangulo

--- main.cpp
+++ main.cpp
@@ -4,21 +4,29 @@
 using namespace std;
 
 int main() {
-    for (int a = 3; a < 4; a++) {
+    for (int a = 3; a < 8; a++) {
         cout << "Introduzca el valor de a, siendo este el primer lado del triangulo: ";
         cin >> a;
-        for (int b = 4; b < 5; b++) {
+        for (int b = 4; b < 15; b++) {
             cout << "Introduzca el valor de b, siendo este el segundo lado del triangulo: ";
             cin >> b;
-            int c = sqrt((a^2) + (b^2));
-            cout << "\nEl valor del tercer lado del triangulo es: " << c << endl;
-            cout << "Lo resolvemos gracias a la formula de Pitagoras" <<endl;
 
+            int c = sqrt((a^2) + (b^2));
+            cout << "El valor de c es: " << c << endl;
+
+            if ( c == floor(c) && a + b > c) {
+                cout << "El triangulo cumple que a + b > c" << endl;
+            }
+            else {
+                cout << "El triangulo no cumple a + b > c" << endl;
+            }
 
 
             return 0;
         }
         return 0;
     }
+
+
     return 0;
 }
