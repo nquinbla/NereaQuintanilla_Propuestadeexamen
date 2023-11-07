92c9332ad290f270c4f9e23741e6cb581bf446dc
nquinbla <nquinbla@myuax.com>
2023-11-07 11:08:29 +0100

    Parte 1 y 2
    no se que estoy haciendo

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    for (int a = 3; a < 4; a++) {
+        cout << "Introduzca el valor de a, siendo este el primer lado del triangulo: ";
+        cin >> a;
+        for (int b = 4; b < 5; b++) {
+            cout << "Introduzca el valor de b, siendo este el segundo lado del triangulo: ";
+            cin >> b;
+
+
+
+
+
+        }
+
+
+    }
+}
